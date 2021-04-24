3e2f7e8afb0474b7fc60b8c1f7e306d8a01b26cd
lordofgod2007 <kabirj.singh@ais.amity.edu>
2021-04-24 13:08:49 +0530

    24/04/21

---
+++ script.js
@@ -0,0 +1,20 @@
+// function odd_and_even(abc){
+// if(abc%2==0){
+//     console.log("Its an even number")
+
+// }
+// else{
+//     console.log("Its an odd number")
+// }
+// }
+// odd_and_even(500);
+
+var a = 1//re declare and reassign
+let b = 2//reassign
+const c = 3//u cant redeclare or reassign
+// var a = 2
+//  b = 5
+//  c = 6
+ {const abc = "Name"}
+
+ console.log("I have /"8/" machines")
