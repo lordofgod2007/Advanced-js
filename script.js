db3511fda4b05a5f5c51c4e42139bd1bcb0100ef
lordofgod2007 <kabirj.singh@ais.amity.edu>
2021-04-25 13:16:43 +0530

    25/04/21

--- script.js
+++ script.js
@@ -10,11 +10,95 @@
 // odd_and_even(500);
 
 var a = 1//re declare and reassign
+var a
 let b = 2//reassign
 const c = 3//u cant redeclare or reassign
 // var a = 2
 //  b = 5
 //  c = 6
- {const abc = "Name"}
+//  const abc = "Name"}
+var name = ["dog","cat","monkey","lion",1,[4,5]]
 
- console.log("I have /"8/" machines")
+// name.push(5);//adds an element in the last
+// console.log(name);
+// name.pop()//removes the last element
+// console.log(name)
+// name.unshift(33)//adda an element in the first
+// console.log(name)
+// name.shift();//removes first element
+// console.log(name)
+
+// name[2] = "tiger"
+// console.log(name)
+// ----------------------------------------------------------------------
+//it ia telling all the even numbers between 1 and 100
+// function is_even(even){
+// if(even%2==0){
+//     return(true);
+// }
+// else{
+//     return(false)
+// }
+// }
+// // console.log(is_even(8));
+
+// var even_num = []
+// for(let i = 1;i<=100;i++){
+// if(is_even(i)){
+//     even_num.push(i);
+// }
+// }
+// console.log(even_num)
+// --------------------------------------------------------------------------
+
+//tell wheather a number is prime or not//formula a muber is priime or not (number +1 /6)//second (formula number-1/6)
+// function is_prime(prime){
+//     if(prime==1){
+//         return(false)
+    
+
+//     }
+//     if(prime==2){
+//         return(true)
+//     }
+   
+//     for(let i =2;i<=(prime**0.5);i++){
+//         if(prime%i==0){
+//             return(false)
+//         }
+//     }
+//     return(true)
+// }
+// console.log(is_prime(27));
+// -----------------------------------------------------------------------------------
+
+// function binaries(x){//this tell the binarie character of a number
+//     if (x==0){
+//     return("0")
+//     }
+// var a = "";
+// while(x != 0)
+// {
+//     var temp = x%2;
+//     a = temp + a;
+//     x = Math.floor(x/2);
+// }
+    
+//         return(a);
+
+// }
+
+// for(let i=1; i<=10; i++)
+// {
+//     console.log(binaries(i));
+// }
+
+//______________-----------------------------------------------------------------
+
+// if((number&1)==1)(
+//     console.log("odd")
+// )
+// else{
+//     console.log("even")
+// }
+//  _____________________________________________________________________________
